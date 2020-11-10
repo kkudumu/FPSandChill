7ccdd9bce93df90c46e3309e5d3c76c9f7f2fb48
kkudumu <kkudumu@gmail.com>
2020-11-10 10:09:42 -0800

    added player shooting abilities, fleshed out more movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier, jumpPower, runSpeed = 12f;
     public CharacterController charCon;
 
     private Vector3 moveInput;
 
     public Transform camTrans;
+
+    public float mouseSensitivity;
+    public bool invertX;
+    public bool invertY;
+
+    private bool canJump, canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
+
+    public GameObject bullet;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +33,83 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        // moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        charCon.Move(moveInput);
+        //store y velocity
+        float yStore = moveInput.y;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput = moveInput * runSpeed;
+        } else
+        {
+            moveInput = moveInput * moveSpeed;
+        }
+
+       
+
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if (charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+      //  if (canJump)
+      //  {
+      //   canDoubleJump = false;
+      //  }
+
+        //Handle Jumping
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            moveInput.y = jumpPower;
+
+            canDoubleJump = true;
+        } else if(canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveInput.y = jumpPower;
+            canDoubleJump = false;
+        }
+
+
+        charCon.Move(moveInput * Time.deltaTime);
 
         //control camera rotation
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        if (invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
         camTrans.rotation = Quaternion.Euler(camTrans.transform.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+
+        //Handle Shooting
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
+
+
+        anim.SetFloat("moveSpeed", moveInput.magnitude);
+        anim.SetBool("onGround", canJump);
     }
 }
